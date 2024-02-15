e55aabaf294361e00def9ebffb7fee14f75f8e1d
Rein van der Woerd <reinvanderwoerd@me.com>
2024-02-15 16:52:19 +0100

    First visual static demo done, with black circle

--- nds_framebuffer_bitmaps/source/main.cpp
+++ nds_framebuffer_bitmaps/source/main.cpp
@@ -1,9 +1,10 @@
 #include <random>
+#include <nds.h>
 #include "../../effolkronium/random.hpp"
 
 
 // Screen stuff -------------------------------------------------
-typedef volatile unsigned int vu32;
+// typedef volatile unsigned int vu32;
 typedef unsigned short u16;
 typedef volatile unsigned short vu16;
 typedef volatile unsigned char vu8;
@@ -18,6 +19,7 @@ typedef volatile unsigned char vu8;
 
 // COLOR uses numbers from (0-31) for each of the RGB components
 #define COLOR(r,g,b)  ((r) | (g)<<5 | (b)<<10)
+#define HSL(h,s,l)    (COLOR(0,0,0) | (h)<<10 | (s)<<5 | (l))
 #define OFFSET(r,c,w) ((r)*(w)+(c))
 
 #define SCREENWIDTH  (256)
@@ -30,6 +32,17 @@ void waitForVblank() {
 }
 
 
+int palette[] = {
+    COLOR(0,0,0), 
+    COLOR(31,31,31), 
+    COLOR(31,0,0), 
+    COLOR(0,31,0), 
+    COLOR(0,0,31),
+    COLOR(31,31,0), 
+    COLOR(31,0,31), 
+    COLOR(0,31,31), 
+    COLOR(31,31,31)
+};
 
 // Random --------------------------------------------------------
 using Random = effolkronium::random_static;
@@ -55,7 +68,7 @@ void initialize_ball() {
     ball.size = 5;
     ball.rdel= 1;
     ball.cdel = 2;
-    ball.color = COLOR(31,31,31);
+    ball.color = COLOR(0,0,0); //COLOR(31,31,31);
 }
 
 
@@ -76,6 +89,75 @@ void drawRect(int row, int col, int width, int height, u16 color) {
     }
 }
 
+void drawTriangleFilled(int x1, int y1, int x2, int y2, int x3, int y3, u16 color) {
+    int min_x = std::min(x1, std::min(x2, x3));
+    int max_x = std::max(x1, std::max(x2, x3));
+    int min_y = std::min(y1, std::min(y2, y3));
+    int max_y = std::max(y1, std::max(y2, y3));
+
+    for (int y = min_y; y <= max_y; y++) {
+        for (int x = min_x; x <= max_x; x++) {
+            if (x1 == x2 && x2 == x3) {
+                if (x == x1) {
+                    setPixel(x, y, color);
+                }
+            } else if (y1 == y2 && y2 == y3) {
+                if (y == y1) {
+                    setPixel(x, y, color);
+                }
+            } else {
+                int x1_x2 = x1 - x2;
+                int x2_x3 = x2 - x3;
+                int x3_x1 = x3 - x1;
+                int y1_y2 = y1 - y2;
+                int y2_y3 = y2 - y3;
+                int y3_y1 = y3 - y1;
+
+                int x_x1 = x - x1;
+                int x_x2 = x - x2;
+                int x_x3 = x - x3;
+                int y_y1 = y - y1;
+                int y_y2 = y - y2;
+                int y_y3 = y - y3;
+
+                int a = x1_x2 * y_y1 - y1_y2 * x_x1;
+                int b = x2_x3 * y_y2 - y2_y3 * x_x2;
+                int c = x3_x1 * y_y3 - y3_y1 * x_x3;
+
+                if ((a >= 0 && b >= 0 && c >= 0) || (a <= 0 && b <= 0 && c <= 0)) {
+                    setPixel(x, y, color);
+                }
+            }
+        }
+    }
+}
+
+void drawCircleFilled(int center_y, int center_x, int radius, u16 color) {
+    int x = 0;
+    int y = radius;
+    int d = 1 - radius;
+
+    while (x <= y) {
+        for (int i = center_x - x; i <= center_x + x; i++) {
+            setPixel(i, center_y + y, color); // Upper half
+            setPixel(i, center_y - y, color); // Lower half
+        }
+
+        for (int i = center_x - y; i <= center_x + y; i++) {
+            setPixel(i, center_y + x, color); // Right half
+            setPixel(i, center_y - x, color); // Left half
+        }
+
+        x++;
+
+        if (d < 0) {
+            d += 2 * x + 1;
+        } else {
+            y--;
+            d += 2 * (x - y) + 1;
+        }
+    }
+}
 
 
 
@@ -109,24 +191,24 @@ void update() {
 
 void draw() {
     // slightly erase everything 
-    // int randomPixel = Random::get(0, SCREENHEIGHT*SCREENWIDTH);
-    // for (int i=0; i<200; i++) {
-    //     VRAM_A[randomPixel] = COLOR(31,31,31);
-    // }
-    
-    // slowwww
-    for (int i=0; i<SCREENHEIGHT*SCREENWIDTH; i++) {
-        // chance of 5%
-        if (rand()%20 == 0) {
-            VRAM_A[i] = COLOR(0,0,0);
-        }
+    for (int i=0; i<100; i++) {
+        int randomPixel = Random::get(0, SCREENHEIGHT*SCREENWIDTH);
+        int randomColor = Random::get(0, 9);
+        VRAM_A[randomPixel] = palette[randomColor];
+        // VRAM_A[randomPixel] = COLOR(0,0,0);
     }
 
     // erase the ball
     // drawRect(old_ball.row, old_ball.col, old_ball.size, old_ball.size, 0);
 
     // draw it in its new position
-    drawRect(ball.row, ball.col, ball.size, ball.size, ball.color);
+    int randomRectColor = Random::get(0, 9);
+    drawRect(ball.row, ball.col, ball.size, ball.size, palette[randomRectColor]);
+    drawRect(ball.row, ball.col, ball.size, ball.size, COLOR(0,0,0));
+
+    int randomColor = Random::get(0, 9);
+    // drawCircleFilled(SCREENWIDTH/2, SCREENHEIGHT/2, 20, palette[randomColor]);
+    drawCircleFilled(SCREENWIDTH/2, SCREENHEIGHT/2, 20, COLOR(0,0,0));
 }
 
 
@@ -143,7 +225,7 @@ int main(void) {
     // main loop
     while(1) {
         update();
-        waitForVblank();
+        // waitForVblank();
         draw();
     }
 
